3da51090966d8d19af550ed846e2621a89e47988
alwaysnikki <1075131@lammersvilleusd.net>
2025-05-07 05:39:08 -0700

    upated unit tests

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,57 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+
+test('isPhoneNumber is true', () => {
+  expect(isPhoneNumber('(987) 123-7898')).toBe(true);
+  expect(isPhoneNumber('408-730-4000')).toBe(true);
+});
+
+test('isPhoneNumber is false', () => {
+  expect(isPhoneNumber('9073427689')).toBe(false);
+  expect(isPhoneNumber('this is a string')).toBe(false);
+});
+
+
+test('isEmail is true', () => {
+  expect(isEmail('m.nikitha@gmail.com')).toBe(true);
+  expect(isEmail('nmaderamitla@gmail.com')).toBe(true);
+});
+
+test('isEmail is false', () => {
+  expect(isEmail('jumble123')).toBe(false);
+  expect(isEmail('user@.com')).toBe(false);
+});
+
+
+test('isStrongPassword is true', () => {
+  expect(isStrongPassword('Notreally123')).toBe(true);
+  expect(isStrongPassword('secondPass_9')).toBe(true);
+});
+
+test('isStrongPassword is false', () => {
+  expect(isStrongPassword('7a8')).toBe(false);
+  expect(isStrongPassword('hi')).toBe(false);
+});
+
+
+test('isDate - true cases', () => {
+  expect(isDate('12/31/2023')).toBe(true);
+  expect(isDate('01/27/1999')).toBe(true);
+});
+
+test('isDate - false', () => {
+  expect(isDate('7890/99/01')).toBe(false);
+  expect(isDate('another-string')).toBe(false);
+});
+
+test('isHexColor - true', () => {
+  expect(isHexColor('#FFFFFF')).toBe(true);
+  expect(isHexColor('#def')).toBe(true);
+});
+
+test('isHexColor - false cases', () => {
+  expect(isHexColor('123456')).toBe(false);
+  expect(isHexColor('string')).toBe(false);
+});
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,47 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+const synth = window.speechSynthesis;
+const voiceSelect = document.getElementById('voice-select');
+const talkButton = document.querySelector('button');
+const textSpeak = document.getElementById('text-to-speak');
+const smileImg = document.querySelector('img');
+let voiceList = [];
+
+function populateVoices() {
+	voiceList = synth.getVoices();
+    	voiceSelect.innerHTML = '';
+    	voiceList.forEach((voice, index) => {
+	      	const option = document.createElement('option');
+      		option.textContent = `${voice.name} (${voice.lang})`;
+      		option.value = index; 
+      		voiceSelect.appendChild(option);
+    	});
 }
+
+populateVoices();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+	speechSynthesis.onvoiceschanged = populateVoices;
+}
+
+talkButton.addEventListener('click', () => {
+    	const text = textSpeak.value;
+    	const utterance = new SpeechSynthesisUtterance(text);
+    	const selectedIndex = parseInt(voiceSelect.value);
+    	if (!isNaN(selectedIndex)) {
+      		utterance.voice = voiceList[selectedIndex];
+    	}
+	smileImg.src = 'assets/images/smiling-open.png';
+
+    	utterance.onend = () => { 
+      		smileImg.src = 'assets/images/smiling.png';
+    	};
+
+    	synth.speak(utterance);
+});
+
+}
+
+
+
+
